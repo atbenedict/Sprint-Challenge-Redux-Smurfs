28b96f932a40ffb0ee11b928eb09d6e64fbac985
Andrew Benedict <atbenedict@gmail.com>
2019-02-01 11:46:32 -0500

    setting up wiring/store/actions/reducers/components

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getSmurfs } from "../actions";
+import SmurfsList from "../components/SmurfsList";
+import SmurfsForm from "../components/SmurfsForm";
+import "./App.css";
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +11,23 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  state = {
+    smurfs: []
+  };
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurfsForm />
+        <SmurfsList />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ state });
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs }
+)(App);
