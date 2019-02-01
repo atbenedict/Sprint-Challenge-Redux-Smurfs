4d875068e171e6563fd8bd43c2734dfb1e674244
Andrew Benedict <atbenedict@gmail.com>
2019-02-01 13:18:31 -0500

    creating smurfs

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -6,7 +6,6 @@ export default function Smurf(props) {
       <h1>{props.name}</h1>
       <h1>{props.age}</h1>
       <h1>{props.height}</h1>
-      <h1>{props.id}</h1>
     </div>
   );
 }
--- smurfs/src/components/SmurfsForm.js
+++ smurfs/src/components/SmurfsForm.js
@@ -1,11 +1,78 @@
 import React from "react";
+import { connect } from "react-redux";
 
-function SmurfsForm(props) {
-  return (
-    <div>
-      <h1>Form goes here...</h1>
-    </div>
-  );
+import { getSmurfs, addSmurf } from "../actions";
+
+class SmurfsForm extends React.Component {
+  state = {
+    smurf: {
+      name: "",
+      age: "",
+      height: ""
+    },
+    isEditing: false
+  };
+
+  handleClick = ev => {
+    ev.preventDefault();
+
+    this.props.addSmurf(this.state.smurf);
+  };
+
+  changeHandler = ev => {
+    this.setState({
+      smurf: {
+        ...this.state.smurf,
+        [ev.target.name]: ev.target.value
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            onChange={this.changeHandler}
+            placeholder="name"
+            value={this.state.smurf.name}
+          />
+          <div className="baseline" />
+
+          <input
+            type="text"
+            placeholder="age"
+            name="age"
+            onChange={this.changeHandler}
+            value={this.state.smurf.age}
+          />
+          <div className="baseline" />
+
+          <input
+            type="text"
+            name="height"
+            onChange={this.changeHandler}
+            placeholder="height"
+            value={this.state.smurf.height}
+          />
+          <div className="baseline" />
+
+          <button onClick={this.handleClick} className="form-button">
+            Add Smurf
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SmurfsForm;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs
+});
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs, addSmurf }
+)(SmurfsForm);
